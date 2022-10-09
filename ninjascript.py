51e740dce3fad06025571fa6adcb1d6bcfd547ce
Keith Flynn <flynn.keith@gmail.com>
2022-10-09 14:37:19 -0400

    created dataframe and second viz for total, removed numpy for merge column average

--- ninjascript.py
+++ ninjascript.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import re
 import matplotlib.pyplot as plt
 
@@ -42,21 +41,37 @@ print(cost)
 
 #df.join(cost)
 test_merge = pd.merge(df, cost)
-#df.append(cost.duplicated().iloc[df.shape[0]:], ignore_index=True)
-avg_merge = test_merge.groupby(['MODELS', 'COUNT']).agg([np.average])
+print(test_merge)
+
+# Group machines by AVERAGE price of model if multiple costs exist
+avg_merge = test_merge.groupby(['MODELS'], as_index=False).mean()
+#avg_merge = test_merge.groupby(['MODELS', 'COUNT']).agg([np.average])
 # Now change float to int
+
+# Making these columns whole numbers
 avg_merge.COST = avg_merge.COST.astype(int)
+avg_merge.COUNT = avg_merge.COUNT.astype(int)
 print(avg_merge)
 
-# 3070 doesn't have a base cost yet. Need to account for this with notification
+# 3070 doesn't have a base cost yet. 
+# It doesn't meet merge criteria and will be discarded when dataframes merge
+# Need to account for this with notification
 no_price = pd.merge(df, cost, how='left', indicator=False)
-#no_price = df[~df['MODELS'].isin(avg_merge.MODELS)]
-#no_price = pd.concat([df['MODELS'], avg_merge['MODELS']]).drop_duplicates(keep=False)
 
 print("**********************************")
 print("*  Machines with no cost basis:  *")
 print("**********************************")
 print(no_price[no_price['COST'].isna()])
+
+# New cost of PO df to make a visualization with lambda fun
+cost_vis = avg_merge
+cost_vis['TOTAL'] = cost_vis.apply(lambda x: x['COUNT'] * x['COST'], axis=1)
+print(cost_vis)
+print("Estimated total PO cost: $", cost_vis['TOTAL'].sum())
+# Make a percentage column for cost_vis df. Each item in total column / total of whole column * 100 to play nice with charting
+cost_vis['TOTALPCT'] = cost_vis.apply(lambda x: (x['TOTAL'] / cost_vis['TOTAL'].sum()) * 100, axis=1)
+print(cost_vis)
+print(cost_vis['TOTALPCT'].sum())
 
 # visuals
 plt.style.use('dark_background')
@@ -65,4 +80,13 @@ plt.bar(df_sorted.MODELS, df_sorted.COUNT)
 plt.xticks(rotation=45, fontsize=7)
 plt.title('Machines Received')
 plt.tight_layout()
+plt.show()
+
+labels = cost_vis['MODELS']
+sizes = cost_vis['TOTALPCT']
+# NA
+explode = (0, 0.1)
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, labels=labels)
 plt.show()
